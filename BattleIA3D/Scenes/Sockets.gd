f85bbe084d8cfd8b2266673bb69e089982f37651
blenderlab <thomas@blenderlab.fr>
2020-09-26 21:46:07 +0200

    working websocket to BattleIA3D

---
+++ BattleIA3D/Scenes/Sockets.gd
@@ -0,0 +1,75 @@
+extends Node
+
+# The URL we will connect to.
+export var websocket_url = "ws://localhost:4626/display"
+
+# Our WebSocketClient instance.
+var _client = WebSocketClient.new()
+
+func _ready():
+	# Connect base signals to get notified of connection open, close, and errors.
+	_client.connect("connection_closed", self, "_closed")
+	_client.connect("connection_error", self, "_closed")
+	_client.connect("connection_established", self, "_connected")
+	# This signal is emitted when not using the Multiplayer API every time
+	# a full packet is received.
+	# Alternatively, you could check get_peer(1).get_available_packets() in a loop.
+	_client.connect("data_received", self, "_on_data")
+
+	# Initiate connection to the given URL.
+	var err = _client.connect_to_url(websocket_url)
+	if err != OK:
+		print("Unable to connect")
+		set_process(false)
+
+
+func _closed(was_clean = false):
+	# was_clean will tell you if the disconnection was correctly notified
+	# by the remote peer before closing the socket.
+	print("Closed, clean: ", was_clean)
+	set_process(false)
+
+
+func _connected(proto = ""):
+	# This is called on connection, "proto" will be the selected WebSocket
+	# sub-protocol (which is optional)
+	print("Connected with protocol: ", proto)
+	# You MUST always use get_peer(1).put_packet to send data to server,
+	# and not put_packet directly when not using the MultiplayerAPI.
+	#_client.get_peer(1).put_packet("Test packet".to_utf8())
+
+
+func _on_data():
+	# Print the received packet, you MUST always use get_peer(1).get_packet
+	# to receive data from server, and not get_packet directly when not
+	# using the MultiplayerAPI.
+	var data =  _client.get_peer(1).get_packet()
+	print(char(data[0]))
+	if char(data[0])=='M':
+		var H = int() 
+		var W =  int()
+
+		H = data[1]
+		W = data[3]
+		print (data[1])
+		print (data[3])
+		var c= 4
+		var d=0
+		get_node('../terrain').set_terrain_height(H)
+		get_node('../terrain').set_terrain_width(W)
+		for i in range(H):
+			for j in range(W):
+				if data[c]==2:
+					get_node('../terrain').addblock(j,i,data[c],d)
+					d+=1
+				c=c+1
+			
+
+func _process(_delta):
+	# Call this in _process or _physics_process. Data transfer, and signals
+	# emission will only happen when calling this function.
+	_client.poll()
+
+
+func _exit_tree():
+	_client.disconnect_from_host()
--- BattleIA3D/Scenes/terrain.gd
+++ BattleIA3D/Scenes/terrain.gd
@@ -25,7 +25,6 @@ func setup_multimesh():
 	
 	self.multimesh.instance_count= terrain_h * terrain_w * 2
 	self.multimesh.visible_instance_count = self.multimesh.instance_count
-	
 	var mesh = CubeMesh.new()
 	mesh.size = Vector3(0.95,0.95,0.95)
 	var material = SpatialMaterial.new()
@@ -39,10 +38,23 @@ func generate_terrain():
 	var origin_distance = 0
 	var origin = Vector3(-self.terrain_w*cube_hs,-self.terrain_h*cube_hs ,0)
 	var index = 0
+	
 	for x in range(self.terrain_w):
 		for y in range(self.terrain_h):
-			var pos = origin + Vector3(x * cube_size, y * cube_size,cube_size)
+			var pos = origin + Vector3(x * cube_size, y * cube_size,0)
 			var color = Color(0.2,0.2,0.2)
 			self.multimesh.set_instance_transform(index,Transform(Basis(),pos) )
 			self.multimesh.set_instance_color(index, color)
 			index += 1
+
+func addblock(_i,_j,_type,_c):
+	var cube_size = 1	
+	var cube_hs = cube_size / 2.0
+	var origin_distance = 0
+	var origin = Vector3(-self.terrain_w*cube_hs,-self.terrain_h*cube_hs ,cube_size)
+	var index = self.terrain_w *self.terrain_h+_c
+	var pos = origin + Vector3(_i * cube_size, _j * cube_size,-2*cube_size)
+	var color = Color(0.1,0.1,0.1)
+	self.multimesh.set_instance_transform(index,Transform(Basis(),pos) )
+	self.multimesh.set_instance_color(index, color)
+
