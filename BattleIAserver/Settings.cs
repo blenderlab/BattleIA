4cb3dc96f0c3941b9439c5da40b3262dbe2f9e32
mentcley <72742704+mentcley@users.noreply.github.com>
2020-10-12 15:03:19 +0200

    Revert "Revert "Merge pull request #7 from rfleuryleveso/endlessmode""
    This reverts commit c0673a648a92dd8c3ff8148abbb625bcca3cd0b0.

--- BattleIAserver/Settings.cs
+++ BattleIAserver/Settings.cs
@@ -19,6 +19,8 @@ namespace BattleIAserver
         public UInt16 EnergyPodLessValue = 1;
         public UInt16 EnergyPodMin = 10;
 
+        public bool EndlessMode = true;
+
         [Newtonsoft.Json.JsonIgnoreAttribute]
         public UInt16 EnergyPodMax;
 
