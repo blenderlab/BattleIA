a0379a77a2055be0d5a011aaaed26cc932258c23
Raphaël Fleury-le veso <56384490+rfleuryleveso@users.noreply.github.com>
2020-10-23 10:25:34 +0200

    Revert "[TODO] Bot : Managing configuration file"

--- SampleBot/Program.cs
+++ SampleBot/Program.cs
@@ -10,6 +10,7 @@ namespace SampleBot
 {
     class Program
     {
+
         private static Settings settings;
 
         static void Main(string[] args)
@@ -32,8 +33,6 @@ namespace SampleBot
             Console.WriteLine("Bye");
             Console.WriteLine("Press [ENTER] to exit.");
             Console.ReadLine();
-
-            var prmBot = Newtonsoft.Json.JsonConvert.DeserializeObject<SettingsBot>(File.ReadAllText(theFile));
         }
 
         private static MyIA ia = new MyIA();
@@ -47,10 +46,10 @@ namespace SampleBot
             // 1 - connect to server
             var serverUrl = $"ws://{settings.ServerHost}:{settings.ServerPort}/bot";
             var client = new ClientWebSocket();
-            Console.WriteLine($"Connecting to {SettingsBot.serverUrl}");
+            Console.WriteLine($"Connecting to {serverUrl}");
             try
             {
-                await client.ConnectAsync(new Uri(SettingsBot.serverUrl), CancellationToken.None);
+                await client.ConnectAsync(new Uri(serverUrl), CancellationToken.None);
             }
             catch (Exception err)
             {
@@ -88,10 +87,8 @@ namespace SampleBot
                                 {
                                     nameIsSent = true;
                                     // sending our name
-
                                     var bName = Encoding.UTF8.GetBytes("N" + settings.BotName);
                                     Console.WriteLine($"Sending our name: {settings.BotName}");
-
                                     await client.SendAsync(new ArraySegment<byte>(bName), WebSocketMessageType.Text, true, CancellationToken.None);
                                     break;
                                 }
