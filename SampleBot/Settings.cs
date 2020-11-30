003d390fa1dfb141352d0c0ada4533fc691afca4
felix  Jallageas <felix.jallageas@student.yncrea.fr>
2020-11-30 14:18:33 +0100

    changement myia

---
+++ SampleBot/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SampleBot
+{
+    public class Settings
+    {
+        public String ServerHost = "127.0.0.1";
+        public int ServerPort = 4626;
+
+        public String BotName = "RandomBot";
+
+    }
+}
